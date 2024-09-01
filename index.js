b61163c8a16640fe90b3c3428667c42891f7ffda
viku4 <vikashkumar04.edugaon@gmail.comm>
2024-09-01 12:12:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_DB_URL)
+app.get('/users', (req, res) => {
+    res.json({
+        "name": "Vikash Kuamar",
+    })
+})
+
+app.listen(3301, () => {
+    console.log('Server is  Running');
+
+})
